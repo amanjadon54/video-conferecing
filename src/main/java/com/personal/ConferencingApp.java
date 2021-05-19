149d08ecc9d248647a87d3732255282d3aa4a7eb
amanjadon54 <amanjadon54@gmail.com>
2021-05-19 14:10:38 +0530

    Core: kurento client for kurento server interaction added

--- src/main/java/com/personal/ConferencingApp.java
+++ src/main/java/com/personal/ConferencingApp.java
@@ -1,4 +1,12 @@
 package com.personal;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ConferencingApp {
+
+    public static void main(String[] s) {
+        SpringApplication.run(ConferencingApp.class);
+    }
 }
---
+++ src/main/java/com/personal/configuration/ConferencingConfiguration.java
@@ -0,0 +1,7 @@
+package com.personal.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConferencingConfiguration {
+}
