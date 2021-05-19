bb8474a16476c4f7893d5f34f21766a188689402
amanjadon54 <amanjadon54@gmail.com>
2021-05-19 16:16:45 +0530

    Core: answer call logic added

--- src/main/java/com/personal/conferencing/handler/CallHandler.java
+++ src/main/java/com/personal/conferencing/handler/CallHandler.java
@@ -2,10 +2,14 @@ package com.personal.conferencing.handler;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.personal.conferencing.model.CallMediaPipeline;
 import com.personal.conferencing.model.UserSession;
 import com.personal.conferencing.registry.UserRegistry;
+import org.kurento.client.EventListener;
 import org.kurento.client.IceCandidate;
+import org.kurento.client.IceCandidateFoundEvent;
 import org.kurento.client.KurentoClient;
+import org.kurento.jsonrpc.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +19,7 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class CallHandler extends TextWebSocketHandler {
 
@@ -27,7 +32,9 @@ public class CallHandler extends TextWebSocketHandler {
     @Autowired
     private UserRegistry userRegistry;
 
+    private final ConcurrentHashMap<String, CallMediaPipeline> pipelines = new ConcurrentHashMap<>();
     private static final Logger log = LoggerFactory.getLogger(CallHandler.class);
+
 
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
@@ -103,15 +110,159 @@ public class CallHandler extends TextWebSocketHandler {
     }
 
     private void call(UserSession caller, JsonObject jsonMessage) throws IOException {
+        String to = jsonMessage.get("to").getAsString();
+        String from = jsonMessage.get("from").getAsString();
+        JsonObject response = new JsonObject();
 
+        if (userRegistry.exists(to)) {
+            UserSession callee = userRegistry.getByName(to);
+            caller.setSdpOffer(jsonMessage.getAsJsonPrimitive("sdpOffer").getAsString());
+            caller.setCallingTo(to);
+
+            response.addProperty("id", "incomingCall");
+            response.addProperty("from", from);
+
+            callee.sendMessage(response);
+            callee.setCallingFrom(from);
+        } else {
+            response.addProperty("id", "callResponse");
+            response.addProperty("response", "rejected: user '" + to + "' is not registered");
+
+            caller.sendMessage(response);
+        }
     }
 
     private void incomingCallResponse(final UserSession callee, JsonObject jsonMessage) throws IOException {
+        String callResponse = jsonMessage.get("callResponse").getAsString();
+        String from = jsonMessage.get("from").getAsString();
+        final UserSession calleer = userRegistry.getByName(from);
+        String to = calleer.getCallingTo();
 
+        if ("accept".equals(callResponse)) {
+            log.debug("Accepted call from '{}' to '{}'", from, to);
+
+            CallMediaPipeline pipeline = null;
+            try {
+                pipeline = new CallMediaPipeline(kurentoClient);
+                pipelines.put(calleer.getSessionId(), pipeline);
+                pipelines.put(callee.getSessionId(), pipeline);
+
+                String calleeSdpOffer = jsonMessage.get("sdpOffer").getAsString();
+                callee.setWebRtcEndpoint(pipeline.getCalleeWebRtcEp());
+                pipeline.getCalleeWebRtcEp().addIceCandidateFoundListener(new EventListener<IceCandidateFoundEvent>() {
+                    @Override
+                    public void onEvent(IceCandidateFoundEvent event) {
+                        JsonObject response = new JsonObject();
+                        response.addProperty("id", "iceCandidate");
+                        response.add("candidate", JsonUtils.toJsonObject(event.getCandidate()));
+                        try {
+                            synchronized (callee.getSession()) {
+                                callee.getSession().sendMessage(new TextMessage(response.toString()));
+                            }
+                        } catch (IOException e) {
+                            log.debug(e.getMessage());
+                        }
+                    }
+                });
+
+                String calleeSdpAnswer = pipeline.generateSdpAnswerForCallee(calleeSdpOffer);
+                String callerSdpOffer = userRegistry.getByName(from).getSdpOffer();
+                calleer.setWebRtcEndpoint(pipeline.getCallerWebRtcEp());
+                pipeline.getCallerWebRtcEp().addIceCandidateFoundListener(new EventListener<IceCandidateFoundEvent>() {
+
+                    @Override
+                    public void onEvent(IceCandidateFoundEvent event) {
+                        JsonObject response = new JsonObject();
+                        response.addProperty("id", "iceCandidate");
+                        response.add("candidate", JsonUtils.toJsonObject(event.getCandidate()));
+                        try {
+                            synchronized (calleer.getSession()) {
+                                calleer.getSession().sendMessage(new TextMessage(response.toString()));
+                            }
+                        } catch (IOException e) {
+                            log.debug(e.getMessage());
+                        }
+                    }
+                });
+
+                String callerSdpAnswer = pipeline.generateSdpAnswerForCaller(callerSdpOffer);
+
+                JsonObject startCommunication = new JsonObject();
+                startCommunication.addProperty("id", "startCommunication");
+                startCommunication.addProperty("sdpAnswer", calleeSdpAnswer);
+
+                synchronized (callee) {
+                    callee.sendMessage(startCommunication);
+                }
+
+                pipeline.getCalleeWebRtcEp().gatherCandidates();
+
+                JsonObject response = new JsonObject();
+                response.addProperty("id", "callResponse");
+                response.addProperty("response", "accepted");
+                response.addProperty("sdpAnswer", callerSdpAnswer);
+
+                synchronized (calleer) {
+                    calleer.sendMessage(response);
+                }
+
+                pipeline.getCallerWebRtcEp().gatherCandidates();
+
+            } catch (Throwable t) {
+                log.error(t.getMessage(), t);
+
+                if (pipeline != null) {
+                    pipeline.release();
+                }
+
+                pipelines.remove(calleer.getSessionId());
+                pipelines.remove(callee.getSessionId());
+
+                JsonObject response = new JsonObject();
+                response.addProperty("id", "callResponse");
+                response.addProperty("response", "rejected");
+                calleer.sendMessage(response);
+
+                response = new JsonObject();
+                response.addProperty("id", "stopCommunication");
+                callee.sendMessage(response);
+            }
+
+        } else {
+            JsonObject response = new JsonObject();
+            response.addProperty("id", "callResponse");
+            response.addProperty("response", "rejected");
+            calleer.sendMessage(response);
+        }
     }
 
     public void stop(WebSocketSession session) throws IOException {
+        String sessionId = session.getId();
+        if (pipelines.containsKey(sessionId)) {
+            pipelines.get(sessionId).release();
+            CallMediaPipeline pipeline = pipelines.remove(sessionId);
+            pipeline.release();
 
+            // Both users can stop the communication. A 'stopCommunication'
+            // message will be sent to the other peer.
+            UserSession stopperUser = userRegistry.getBySession(session);
+            if (stopperUser != null) {
+                UserSession stoppedUser = (stopperUser.getCallingFrom() != null)
+                        ? userRegistry.getByName(stopperUser.getCallingFrom())
+                        : stopperUser.getCallingTo() != null
+                        ? userRegistry.getByName(stopperUser.getCallingTo())
+                        : null;
+
+                if (stoppedUser != null) {
+                    JsonObject message = new JsonObject();
+                    message.addProperty("id", "stopCommunication");
+                    stoppedUser.sendMessage(message);
+                    stoppedUser.clear();
+                }
+                stopperUser.clear();
+            }
+
+        }
     }
 
     @Override
---
+++ src/main/java/com/personal/conferencing/model/CallMediaPipeline.java
@@ -0,0 +1,45 @@
+package com.personal.conferencing.model;
+
+import lombok.Getter;
+import org.kurento.client.KurentoClient;
+import org.kurento.client.MediaPipeline;
+import org.kurento.client.WebRtcEndpoint;
+
+public class CallMediaPipeline {
+
+    private MediaPipeline pipeline;
+    @Getter
+    private WebRtcEndpoint callerWebRtcEp;
+    @Getter
+    private WebRtcEndpoint calleeWebRtcEp;
+
+    public CallMediaPipeline(KurentoClient kurento) {
+        try {
+            this.pipeline = kurento.createMediaPipeline();
+            this.callerWebRtcEp = new WebRtcEndpoint.Builder(pipeline).build();
+            this.calleeWebRtcEp = new WebRtcEndpoint.Builder(pipeline).build();
+
+            this.callerWebRtcEp.connect(this.calleeWebRtcEp);
+            this.calleeWebRtcEp.connect(this.callerWebRtcEp);
+        } catch (Throwable t) {
+            if (this.pipeline != null) {
+                pipeline.release();
+            }
+        }
+    }
+
+    public String generateSdpAnswerForCaller(String sdpOffer) {
+        return callerWebRtcEp.processOffer(sdpOffer);
+    }
+
+    public String generateSdpAnswerForCallee(String sdpOffer) {
+        return calleeWebRtcEp.processOffer(sdpOffer);
+    }
+
+    public void release() {
+        if (pipeline != null) {
+            pipeline.release();
+        }
+    }
+
+}
